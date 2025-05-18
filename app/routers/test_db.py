1dbb3a4e3b01359cf17f036222a18c0295fe2920
Konstantin Sandanov <kostyansandanov@gmail.com>
2025-05-18 09:10:15 +0800

    feat: добавлено тестовый роутер для подлючение к базе

--- app/routers/test_db.py
+++ app/routers/test_db.py
@@ -4,6 +4,7 @@ from app.db.deps import get_db
 
 router = APIRouter()
 
+# тестовый роутер для проверки БД
 @router.get("/ping-db")
 def ping_db(db: Session = Depends(get_db)):
     return{"message":"База подключена и работает."}
