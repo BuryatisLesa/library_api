c0a70b75bb70bca74a6b801353f3519f0748d06f
Konstantin Sandanov <kostyansandanov@gmail.com>
2025-05-19 04:27:02 +0800

    feat: добавлена защита JWT для books and readers

--- app/routers/books.py
+++ app/routers/books.py
@@ -1,16 +1,22 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from typing import List
+
 from app.db.deps import get_db
 from app.models.book import Book
 from app.schemas.book import BookCreate, BookOut, BookUpdate
-from typing import List
-
+from app.core.deps import get_current_user
+from app.models.user import User
 
 router = APIRouter()
 
 # создание новой книги
 @router.post("/books", response_model=BookOut)
-def create_book(book: BookCreate, db: Session = Depends(get_db)):
+def create_book(
+    book: BookCreate,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
     new_book = Book(**book.dict())
     db.add(new_book)
     db.commit()
@@ -19,12 +25,19 @@ def create_book(book: BookCreate, db: Session = Depends(get_db)):
 
 # получение все имеющих книг
 @router.get("/books", response_model=List[BookOut])
-def get_books(db: Session = Depends(get_db)):
+def get_books(
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
     return db.query(Book).all()
 
 # получение одной книги
 @router.get("/books/{book_id}", response_model=BookOut)
-def get_book(book_id: int, db: Session = Depends(get_db)):
+def get_book(
+    book_id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
     book = db.query(Book).filter(Book.id == book_id).first()
 
     if not book:
@@ -33,7 +46,11 @@ def get_book(book_id: int, db: Session = Depends(get_db)):
 
 # удлаение книги
 @router.delete("/books/{book_id}", response_model=BookOut)
-def delete_book(book_id: int, db: Session = Depends(get_db)):
+def delete_book(
+    book_id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
     book = db.query(Book).filter(Book.id == book_id).first()
     if not book:
         raise HTTPException(status_code=404, detail="Книга не найдена")
@@ -45,17 +62,19 @@ def delete_book(book_id: int, db: Session = Depends(get_db)):
 
 # обновление данных книги
 @router.put("/books/{book_id}", response_model=BookOut)
-def update_book(book_id:int, update_data: BookUpdate, db: Session = Depends(get_db)):
+def update_book(
+    book_id: int,
+    update_data: BookUpdate,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
     book = db.query(Book).filter(Book.id == book_id).first()
     if not book:
         raise HTTPException(status_code=404, detail="Книга не найдена")
     
-
     for field, value in update_data.dict(exclude_unset=True).items():
         setattr(book, field, value)
-    
 
     db.commit()
     db.refresh(book)
     return book
-
--- app/routers/readers.py
+++ app/routers/readers.py
@@ -1,32 +1,43 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from typing import List
+
 from app.db.deps import get_db
 from app.models.reader import Reader
 from app.schemas.reader import ReaderCreate, ReaderOut, ReaderUpdate
-from typing import List
-
+from app.core.deps import get_current_user
+from app.models.user import User
 
 router = APIRouter()
 
 # создание нового читателя
 @router.post("/readers", response_model=ReaderOut)
-def create_reader(reader: ReaderCreate, db: Session = Depends(get_db)):
-
+def create_reader(
+    reader: ReaderCreate,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
     new_reader = Reader(**reader.dict())
     db.add(new_reader)
     db.commit()
     db.refresh(new_reader)
-
     return new_reader
 
 # получение всех имеющих читателей
 @router.get("/readers", response_model=List[ReaderOut])
-def get_readers(db: Session = Depends(get_db)):
+def get_readers(
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
     return db.query(Reader).all()
 
-# получение одно читателя
+# получение одного читателя
 @router.get("/readers/{reader_id}", response_model=ReaderOut)
-def get_reader(reader_id: int, db: Session = Depends(get_db)):
+def get_reader(
+    reader_id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
     reader = db.query(Reader).filter(Reader.id == reader_id).first()
     if not reader:
         raise HTTPException(status_code=404, detail="Читатель не найден")
@@ -34,27 +45,33 @@ def get_reader(reader_id: int, db: Session = Depends(get_db)):
 
 # удаление читателя
 @router.delete("/readers/{reader_id}", response_model=ReaderOut)
-def delete_reader(reader_id: int, db: Session = Depends(get_db)):
+def delete_reader(
+    reader_id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
     reader = db.query(Reader).filter(Reader.id == reader_id).first()
     if not reader:
         raise HTTPException(status_code=404, detail="Читатель не найден")
-    
     db.delete(reader)
-    db.commit
-
+    db.commit()
     return reader
 
 # обновление данных читателя
-@router.put("/readers/{reader_id},", response_model=ReaderOut)
-def update_reader(reader_id: int, update_data: ReaderUpdate, db: Session = Depends(get_db)):
+@router.put("/readers/{reader_id}", response_model=ReaderOut)
+def update_reader(
+    reader_id: int,
+    update_data: ReaderUpdate,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
     reader = db.query(Reader).filter(Reader.id == reader_id).first()
     if not reader:
         raise HTTPException(status_code=404, detail="Читатель не найден")
-    
+
     for field, value in update_data.dict(exclude_unset=True).items():
         setattr(reader, field, value)
 
     db.commit()
     db.refresh(reader)
-
     return reader
