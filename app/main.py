36599bb7d2bcdccfc2a042a7e929b7c3e51749aa
Konstantin Sandanov <kostyansandanov@gmail.com>
2025-05-18 10:14:26 +0800

    test: эндпоинт регистрации успешно проведен

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
-from app.routers import test_db
+from app.routers import test_db, auth
 from fastapi import FastAPI
 
 app = FastAPI()
 
 app.include_router(test_db.router, tags=["Test DB"])
+
+app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
