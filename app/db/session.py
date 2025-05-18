31d71581ebe101e322291e238bfc76556cdd8f50
Konstantin Sandanov <kostyansandanov@gmail.com>
2025-05-18 10:08:26 +0800

    fix: был исправлен engine была опечатка в connect_args: check_same_tread => check_same_thread*

--- app/db/session.py
+++ app/db/session.py
@@ -8,7 +8,7 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./library.db"
 # создаем соединение с sqlite
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_tread":False}
+    connect_args={"check_same_thread":False}
     )
 
 # создаем шаблон для подключение к БД при каждом запросе
