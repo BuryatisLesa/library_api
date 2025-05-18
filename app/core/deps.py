4ed632369ffbb6130567a1f91ce979d21659cc26
Konstantin Sandanov <kostyansandanov@gmail.com>
2025-05-18 21:28:49 +0800

    feat: создана функция для расшифровки JWT

---
+++ app/core/deps.py
@@ -0,0 +1,38 @@
+from fastapi import HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWSError
+from sqlalchemy.orm import Session
+from app.db.deps import get_db
+from app.models.user import User
+from app.core.security import SECRET_KEY, ALGORITHM
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+
+def get_current_user(
+        token: str = Depends(oauth2_scheme),
+        db: Session = Depends(get_db),
+        credentials_exception = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail = "Невалидный токен",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+):
+    # расшифровка JWT
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+
+        if email is None:
+            raise credentials_exception
+    except JWSError:
+        raise credentials_exception
+    
+    # проверка на наличие пользователя, а также получение его
+    user = db.query(User).filter(User.email == email).first()
+    if user is None:
+        raise credentials_exception
+    
+
+    return user
